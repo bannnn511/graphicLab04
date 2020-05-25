885c5c1e3f3c2646ca994b9881e8af2970927dce
An <habinhan1999@gmail.com>
2020-05-25 13:45:52 +0700

    Initial Commit

---
+++ Lab-04/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab-04
+//
+//  Created by An on 5/25/20.
+//  Copyright © 2020 An Binh Ha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
